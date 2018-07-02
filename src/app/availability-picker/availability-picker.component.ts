813e3a702270da2d107a7a5b61ea7252f4d166a3
Jordan Bonser <jordan_bonser@live.co.uk>
2018-07-02 21:30:33 +0100

    Fixed Month Switching Bug
    There was an issue where the beforeViewRender function
    wouldn't properly identify dates once you had moved months.
    This is now fixed by using Date.getTime for comparison.

--- src/app/availability-picker/availability-picker.component.ts
+++ src/app/availability-picker/availability-picker.component.ts
@@ -7,10 +7,11 @@ import { CalendarEvent, CalendarMonthViewDay } from 'angular-calendar';
   templateUrl: './availability-picker.component.html',
   styleUrls: ['./availability-picker.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AvailabilityPickerComponent implements OnInit {
   viewDate: Date = new Date();
-  viewDates: CalendarMonthViewDay[] = [];
+  viewDates: Date[] = [];
   selectedDate: CalendarMonthViewDay;
   view = 'month';
 
@@ -20,36 +21,28 @@ export class AvailabilityPickerComponent implements OnInit {
   ngOnInit() {}
 
   dayClicked(day: CalendarMonthViewDay): void {
-    const index =  this.viewDates.indexOf(day);
+    const index =  this.viewDates.indexOf(day.date);
     if (index !== -1) {
        this.viewDates.splice(index, 1);
        delete day.cssClass;
     } else {
       const newDay: CalendarMonthViewDay = day;
-      this.viewDates.push(newDay);
+      this.viewDates.push(day.date);
       newDay.cssClass = 'cal-day-selected';
     }
   }
 
-  beforeMonthViewRender2({ body }: { body: CalendarMonthViewDay[] }): void {
-    body.forEach(day => {
-      if ( this.viewDates &&
-           this.viewDates.indexOf(day) !== -1) {
-        day.cssClass = 'cal-day-selected';
-        this.selectedDate = day;
-      }
-    });
-  }
-
   beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
+    console.log(this.viewDates)
     body.forEach(day => {
-      if (
-        this.selectedDate &&
-        day.date.getTime() === this.selectedDate.date.getTime()
-      ) {
-        day.cssClass = 'cal-day-selected';
-        this.selectedDate = day;
-      }
-    });
+      if (this.viewDates) {
+        this.viewDates.forEach(viewDate => {
+          // Comparison only work with getTime as that uses milliseconds
+          if (viewDate.getTime() === day.date.getTime()) {
+          day.cssClass = 'cal-day-selected';
+          this.selectedDate = day;
+        }
+      });
+    }
   }
 }
